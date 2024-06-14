19e5211bf1b11be9f30c9c9e59370505410ba602
lyl <lyl@kags.com>
2024-06-14 09:51:05 +0800

    feat: auto pack uniappx

--- uniappx/src/main/java/pages/index.kt
+++ uniappx/src/main/java/pages/index.kt
@@ -26,7 +26,7 @@ open class GenPagesIndexIndex : BasePage {
             val __ins = getCurrentInstance()!!;
             val _ctx = __ins.proxy as GenPagesIndexIndex;
             val _cache = __ins.renderCache;
-            val title = ref("Hello11");
+            val title = ref("Hello");
             val plus = IPlus(accelerometer = useAccelerometer());
             val getCurrentAcceleration = fun(){
                 plus.accelerometer.getCurrentAcceleration(fun(a){
